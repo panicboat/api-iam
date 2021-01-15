bca1cd4b1694bc48edf254c45f81d004fdccd7b8
panicboat <panicboat@gmail.com>
2021-01-16 01:04:02 +0900

    Fix test for Permission API

--- test/concepts/permissions/index_test.rb
+++ test/concepts/permissions/index_test.rb
@@ -2,20 +2,14 @@ require 'test_helper'
 
 module Permissions
   class IndexTest < ActionDispatch::IntegrationTest
-    setup do
-      service1 = ::Services::Operation::Create.call(params: { name: 'iam' })
-      service2 = ::Services::Operation::Create.call(params: { name: 'spec' })
-      ::Actions::Operation::Create.call(params: { service_id: service1[:model].id, name: 'IamAction', access_level: 'write' })
-      ::Actions::Operation::Create.call(params: { service_id: service2[:model].id, name: 'SpecAction', access_level: 'write' })
-      @user = ::Users::Operation::Create.call({ params: { email: 'dummy@panicboat.net', name: 'Spec' } })
-      @group1 = ::Groups::Operation::Create.call(params: { name: 'group1' })
-      @group2 = ::Groups::Operation::Create.call(params: { name: 'group2' })
-      @policy1 = ::Policies::Operation::Create.call(params: { name: 'SpecPolicy1', statements: [{ effect: 'allow', actions: ['iam:*'], resources: ['prn:panicboat:platform:iam:group/bc2edb53-551c-*'] }] })
-      @policy2 = ::Policies::Operation::Create.call(params: { name: 'SpecPolicy2', statements: [{ effect: 'allow', actions: ['spec:*'], resources: ['*'] }] })
-      @role1 = ::Roles::Operation::Create.call(params: { name: 'SpecRole1' })
-      @role2 = ::Roles::Operation::Create.call(params: { name: 'SpecRole2' })
-      ::MapRolePolicies::Operation::Create.call(params: { role_id: @role1[:model].id, policy_id: @policy1[:model].id })
-      ::MapRolePolicies::Operation::Create.call(params: { role_id: @role2[:model].id, policy_id: @policy2[:model].id })
+    fixtures :users, :groups, :roles, :policies
+    fixtures :services, :actions
+    fixtures :statements, :resources, :map_statement_actions
+
+    def setup
+      @permissions = ['standalone:GetSpec', 'standalone:CreateSpec', 'standalone:UpdateSpec', 'standalone:DeleteSpec', 'standalone:ListSpec']
+      ::MapGroupUser.new(group_id: groups(:standalone).id, user_id: users(:standalone).id).save!
+      ::MapRolePolicy.new(role_id: roles(:standalone).id, policy_id: policies(:standalone).id).save!
     end
 
     def default_params
@@ -27,46 +21,38 @@ module Permissions
     end
 
     test 'Check UserRole' do
-      ::MapUserRoles::Operation::Create.call(params: { user_id: @user[:model].id, role_id: @role1[:model].id })
-      ::MapUserRoles::Operation::Create.call(params: { user_id: @user[:model].id, role_id: @role2[:model].id })
-      ctx = Operation::Index.call(params: {}, current_user: @user[:model])
-      assert_equal ctx[:model].Permissions.length, 2
+      ::MapUserRole.new(user_id: users(:standalone).id, role_id: roles(:standalone).id).save!
+      ctx = Operation::Index.call(params: {}, current_user: users(:standalone))
+      assert_equal 5, ctx[:model].Permissions.length
       ctx[:model].Permissions.each do |permission|
-        assert_equal ['iam:IamAction', 'spec:SpecAction'].include?(permission), true
+        assert_equal true, @permissions.include?(permission)
       end
     end
 
     test 'Check UserPolicy' do
-      ::MapUserPolicies::Operation::Create.call(params: { user_id: @user[:model].id, policy_id: @policy1[:model].id })
-      ::MapUserPolicies::Operation::Create.call(params: { user_id: @user[:model].id, policy_id: @policy2[:model].id })
-      ctx = Operation::Index.call(params: {}, current_user: @user[:model])
-      assert_equal ctx[:model].Permissions.length, 2
+      ::MapUserPolicy.new(user_id: users(:standalone).id, policy_id: policies(:standalone).id).save!
+      ctx = Operation::Index.call(params: {}, current_user: users(:standalone))
+      assert_equal 5, ctx[:model].Permissions.length
       ctx[:model].Permissions.each do |permission|
-        assert_equal ['iam:IamAction', 'spec:SpecAction'].include?(permission), true
+        assert_equal true, @permissions.include?(permission)
       end
     end
 
     test 'Check GroupRole' do
-      ::MapGroupUsers::Operation::Create.call(params: { group_id: @group1[:model].id, user_id: @user[:model].id })
-      ::MapGroupUsers::Operation::Create.call(params: { group_id: @group2[:model].id, user_id: @user[:model].id })
-      ::MapGroupRoles::Operation::Create.call(params: { group_id: @group1[:model].id, role_id: @role1[:model].id })
-      ::MapGroupRoles::Operation::Create.call(params: { group_id: @group2[:model].id, role_id: @role2[:model].id })
-      ctx = Operation::Index.call(params: {}, current_user: @user[:model])
-      assert_equal ctx[:model].Permissions.length, 2
+      ::MapGroupRole.new(group_id: groups(:standalone).id, role_id: roles(:standalone).id).save!
+      ctx = Operation::Index.call(params: {}, current_user: users(:standalone))
+      assert_equal 5, ctx[:model].Permissions.length
       ctx[:model].Permissions.each do |permission|
-        assert_equal ['iam:IamAction', 'spec:SpecAction'].include?(permission), true
+        assert_equal true, @permissions.include?(permission)
       end
     end
 
     test 'Check GroupPolicy' do
-      ::MapGroupUsers::Operation::Create.call(params: { group_id: @group1[:model].id, user_id: @user[:model].id })
-      ::MapGroupUsers::Operation::Create.call(params: { group_id: @group2[:model].id, user_id: @user[:model].id })
-      ::MapGroupPolicies::Operation::Create.call(params: { group_id: @group1[:model].id, policy_id: @policy1[:model].id })
-      ::MapGroupPolicies::Operation::Create.call(params: { group_id: @group2[:model].id, policy_id: @policy2[:model].id })
-      ctx = Operation::Index.call(params: {}, current_user: @user[:model])
-      assert_equal ctx[:model].Permissions.length, 2
+      ::MapGroupPolicy.new(group_id: groups(:standalone).id, policy_id: policies(:standalone).id).save!
+      ctx = Operation::Index.call(params: {}, current_user: users(:standalone))
+      assert_equal 5, ctx[:model].Permissions.length
       ctx[:model].Permissions.each do |permission|
-        assert_equal ['iam:IamAction', 'spec:SpecAction'].include?(permission), true
+        assert_equal true, @permissions.include?(permission)
       end
     end
   end
