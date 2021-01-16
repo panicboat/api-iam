433f81dd3223e3aed86f9893d0e64c8c81b3b727
panicboat <panicboat@gmail.com>
2021-01-16 11:51:29 +0900

    Update show.rb

--- app/concepts/actions/contract/show.rb
+++ app/concepts/actions/contract/show.rb
@@ -1,11 +1,9 @@
 module Actions::Contract
   class Show < Abstract::Contract
-    class Show < Abstract::Contract
-      property  :id
-      property  :service_id
+    property  :id
+    property  :service_id
 
-      validates :id,          presence: true, format: { with: FORMAT_UUID }
-      validates :service_id,  presence: true, format: { with: FORMAT_UUID }
-    end
+    validates :id,          presence: true, format: { with: FORMAT_UUID }
+    validates :service_id,  presence: true, format: { with: FORMAT_UUID }
   end
 end
